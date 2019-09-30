14af67f7a015db9eebb5d52660cac95831c06449
Thibault Goudouneix <nilmanduil@gmail.com>
2019-09-30 18:51:30 +0200

    Init Timer component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
+	import Timer from "./Timer.svelte";
 	export let name;
 </script>
 
 <style>
 	h1 {
 		color: purple;
+		text-align: center;
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>{name}</h1>
+<Timer />
---
+++ src/Timer.svelte
@@ -0,0 +1,37 @@
+<script>
+    const SECONDS_IN_MINUTES = 60;
+    export let workDuration = 25 * SECONDS_IN_MINUTES * 1000; // Duration of a work session in milliseconds
+    export let pauseDuration = 5 * SECONDS_IN_MINUTES * 1000; // Duration of a pause session in milliseconds
+
+    let isTimerPaused = false;
+
+    let currentPhase = "waiting";
+    let currentPhaseDuration;
+    let startTime;
+
+    let remainingTime = workDuration;
+
+    function resetTimer() {
+        isTimerPaused = false;
+        currentPhase = "waiting";
+        currentPhaseDuration = undefined;
+        remainingTime = workDuration;
+    }
+
+    function pauseTimer() {
+        isTimerPaused = true;
+    }
+
+    function resumeTimer() {
+        // TODO
+        isTimerPaused = false;
+    }
+</script>
+
+<h2>{remainingTime}</h2>
+<button on:click={resetTimer}>Stop</button>
+{#if isTimerPaused}
+    <button on:click={resumeTimer}>Start</button>
+{:else}
+    <button on:click={pauseTimer}>Pause</button>
+{/if}
