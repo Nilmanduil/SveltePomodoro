6288b977ee00c7953589c5104349fb92e60e79ae
Thibault Goudouneix <nilmanduil@gmail.com>
2019-10-05 08:49:46 +0200

    Init timer

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,13 +1,14 @@
 <script>
     const SECONDS_IN_MINUTES = 60;
-    export let workDuration = 25 * SECONDS_IN_MINUTES * 1000; // Duration of a work session in milliseconds
-    export let breakDuration = 5 * SECONDS_IN_MINUTES * 1000; // Duration of a break session in milliseconds
+    export let workDuration = 25 * SECONDS_IN_MINUTES; // Duration of a work session in seconds
+    export let breakDuration = 5 * SECONDS_IN_MINUTES; // Duration of a break session in seconds
 
     let isTimerPaused = false;
 
     let currentPhase = "waiting";
     let currentPhaseDuration;
     let startTime;
+    let timer;
 
     let remainingTime = workDuration;
 
@@ -15,16 +16,25 @@
         isTimerPaused = false;
         currentPhase = "waiting";
         currentPhaseDuration = undefined;
+        clearInterval(timer);
         remainingTime = workDuration;
     }
 
     function pauseTimer() {
         isTimerPaused = true;
+        clearInterval(timer);
     }
 
     function resumeTimer() {
         // TODO
         isTimerPaused = false;
+        refreshTimer();
+    }
+
+    function refreshTimer() {
+        timer = setInterval(() => {
+            remainingTime -= 1;
+        }, 1000);
     }
 </script>
 
